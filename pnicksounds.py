e0122e3dff55c31903da62eec6887b3108a04694
Christopher Hallinan <challinan@gmail.com>
2012-09-12 15:08:51 -0600

    Added command handler

--- pnicksounds.py
+++ pnicksounds.py
@@ -5,21 +5,15 @@ __module_description__ = "Python module example"
 import os
 import xchat
 
-mynick = "challinan"
+# My first python program - probably 100 better ways to do this...
+# Includes a command handler to enable and disable sounds without unloading the plugin
+# /ns off or /ns on to disable/enable sounds
+
+sounds_on = 1
 play_cmd = "afplay"
 sounds_dir = "/Users/chris/Library/Sounds"
 
 # Associate sounds with channels here
-nick_sound = "c-d-g.wav"
-mel_sound = "mel_sound.wav";
-codebench_sound = "gmorse-n.wav"
-watercooler_sound = "g440-380-drop.wav"
-montavista_sound = "gbullet.wav"
-marketing_sound = "gPurr.aiff"
-plat_sound = "gchirp.wav"
-ae_sound = "gmorse-a.wav"
-other_sound = "g440-short-dot.wav"
-
 sounds_dic = {'#mel' : 'mel_sound.wav',
 			  '#codebench'  : 'morse-n.wav',
 			  '#watercooler'  : '440-380-drop.wav',
@@ -29,31 +23,13 @@ sounds_dic = {'#mel' : 'mel_sound.wav',
 			  '#test' : 'ringout.wav',
 			  '#other' : '440-short-dot.wav'}
 
-# xchat.prnt("This is xchat.prnt")
-
-nick = xchat.get_prefs("irc_nick1")
-print "mynick is %s" % nick
-#if xchat.nickcmp(nick, mynick) == 0: 
-#	print "They are the same!"
-
-# This function retrieves the xchat setting (see /set)
-# print "Current preferred nick:", xchat.get_prefs("irc_nick1")
-
-# This code produces a list of channels
-# channel_list = xchat.get_list("channels") 
-# if channel_list: 
-#     print "--- channel list ------------------" 
-#     for i in channel_list: 
-#        print i.channel
-
-# This code produces a list of users in a channel
-# user_list = xchat.get_list("users")
-# if user_list:
-# 	print "--- user list ---------------------"
-#	for i in user_list:
-# 		print i.nick
+mynick = xchat.get_prefs("irc_nick1")
+print "mynick is %s - sounds are enabled" % mynick
 
 def channel_msg_processor(word, word_eol, userdata):
+	if (sounds_on == 0):
+		return xchat.EAT_NONE
+
 	current_channel = xchat.get_info("channel")
 	current_nick = xchat.get_info("nick")
 	
@@ -69,11 +45,28 @@ def channel_msg_processor(word, word_eol, userdata):
 
 # This hook is called whenever my nick appears in a channel message
 def channel_hilite_processor(word, word_eol, userdata):
+	if (sounds_on == 0):
+		return xchat.EAT_NONE
+
 	sound_cmd = 'afplay ' + sounds_dir + '/'
 	sound_cmd = sound_cmd + 'c-d-g.wav'
 	os.system(sound_cmd)
 	return xchat.EAT_NONE
 
+def do_sound_control(word, word_eol, userdata):
+	global sounds_on
+
+	if ('off' in word):
+		sounds_on = 0
+		print "Nicksounds are OFF"
+		return xchat.EAT_ALL
+
+	if ('on' in word):
+		sounds_on = 1
+		print "Nicksounds are ON"
+		return xchat.EAT_ALL
+
 xchat.hook_print("Channel Message", channel_msg_processor)
 xchat.hook_print("Channel Msg Hilight", channel_hilite_processor)
 xchat.hook_print("Private Message", channel_hilite_processor)
+xchat.hook_command("ns", do_sound_control)
